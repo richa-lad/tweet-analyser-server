e2da29a7b996a553bd4d1fdb98033427e1da8764
Richa Lad <richa.lad.17@ucl.ac.uk>
2022-07-12 16:38:36 +0100

    CORS
    allowed for cross origin access

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from data_collection.cred import API_KEY, API_SECRET, ACCESS_TOKEN, TOKEN_SECRET, BEARER_TOKEN
 from data_collection.RetrieveTweets import create_dataframe
@@ -17,6 +18,20 @@ class User(BaseModel):
 
 # start the app
 app = FastAPI()
+
+# allow for CORS from front end app
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # connect to twitter
 api = twitter.Api(
@@ -82,8 +97,22 @@ async def classify_user_tweets(user: User):
     print("loading weights")
     model.load_weights("checkpoints/training.ckpt")
     print("predicting...")
+
     # get predictions
     predictions = model.predict(x=[X1, X2])
     one_hot = [[1 if max(prediction)==x else 0 for x in prediction] for prediction in predictions]
     processed_to_names = [target_headers[vec.index(1)] for vec in one_hot]
-    return {name: processed_to_names.count(name)/len(processed_to_names) for name in processed_to_names}
+
+    scores_names = {processed_to_names.count(name)/len(processed_to_names):name for name in processed_to_names}
+
+    # return results in easy to consume format for js
+    scores = list(scores_names.keys())
+    scores.sort(reverse=True)
+    results = dict()
+    for i, score in enumerate(scores[:5]):
+        results[f"{i}"] = {
+            "username": scores_names[score],
+            "score": score
+        }
+
+    return results
