fe48ec8a557cc88b3707beff9d79f8737e589247
richa-lad <72407613+richa-lad@users.noreply.github.com>
2023-03-18 20:16:26 +0000

    Update api.py

--- routes/api.py
+++ routes/api.py
@@ -32,8 +32,7 @@ origins = [
     "http://localhost:3000/",
     "http://localhost:3001/",
     "https://rhobh-ta.herokuapp.com",
-    "https://tweet-analyzer.onrender.com/",
-    "https://tweet-analyzer-server.onrender.com/"
+    "https://tweet-analyzer.onrender.com"
 ]
 
 app.add_middleware(
